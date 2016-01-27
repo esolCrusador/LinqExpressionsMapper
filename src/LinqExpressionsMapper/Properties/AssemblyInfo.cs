b3832f47272abf8aad856abdd6237318312a8247
esolCursador <Boris.Sotsky@gmail.com>
2016-01-27 20:40:19 +0300

    *

--- src/LinqExpressionsMapper/Properties/AssemblyInfo.cs
+++ src/LinqExpressionsMapper/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("esolCrusador, Boris.Sotsky@gmail.com")]
 [assembly: AssemblyProduct("LinqExpressionsMapper")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("esolCrusador, Boris.Sotsky@gmail.com")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.3.7")]
-[assembly: AssemblyFileVersion("3.3.7")]
+[assembly: AssemblyVersion("3.4.0")]
+[assembly: AssemblyFileVersion("3.4.0")]
